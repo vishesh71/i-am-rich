ea49e7119d7709aebb851cdfa46a2b250b50ae96
Vishesh Kumar <vk7879@srmist.edu.in>
2022-01-15 18:14:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,17 @@ void main() {
   runApp(
 // Widget Tree
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('I am Rich')),
+          backgroundColor: Colors.blueGrey[900],
+
+        ),
+        backgroundColor: Colors.blueGrey[400],
+        body: Center(child: Image( image:NetworkImage('https://elphickpublishing.co.uk/wp-content/uploads/2021/03/depositphotos_13556034-stock-photo-shiny-diamond-isolated-on-black.jpg'),)
+
+        ),
       ),
-    ),
-  );
-}
+    ));
+  }
+
