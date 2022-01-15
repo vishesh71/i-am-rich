1a8faad2956f019bb536c7c89c1637cc54d692c6
Vishesh Kumar <vk7879@srmist.edu.in>
2022-01-16 00:39:30 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:shake_widget/shake_widget.dart';
+import 'package:shake_widget/controller.dart';
+
 // The main function is the starting point for all out Flutter apps.
 void main() {
+  ShakeController shakeController = ShakeController();
   runApp(
 // Widget Tree
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('I am Rich')),
-          backgroundColor: Colors.blueGrey[900],
 
-        ),
-        backgroundColor: Colors.blueGrey[400],
+      MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('I am Rich')),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey[400],
 
-        //body: Center(child: Image( image:NetworkImage('https://elphickpublishing.co.uk/wp-content/uploads/2021/03/depositphotos_13556034-stock-photo-shiny-diamond-isolated-on-black.jpg'),)
-        body: Center(child: Image(image: AssetImage('images/diamond.png'),),
-
+      //body: Center(child: Image( image:NetworkImage('https://elphickpublishing.co.uk/wp-content/uploads/2021/03/depositphotos_13556034-stock-photo-shiny-diamond-isolated-on-black.jpg'),)
+      body: Center(
+        child: GestureDetector(
+          onTap: (){
+            shakeController.shake();
+          },
+          child: ShakeWidget(
+              child: Image(
+                image: NetworkImage('https://elphickpublishing.co.uk/wp-content/uploads/2021/03/depositphotos_13556034-stock-photo-shiny-diamond-isolated-on-black.jpg'),
+              ),
+              axisDirection: Axis.horizontal,
+              shakeController: shakeController),
         ),
       ),
-    ));
-  }
-
+    ),
+  ));
+}
